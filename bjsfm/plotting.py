de84f6bc15ac596a2858e5d2b981f9acc1525591
BenjaminETaylor <benjamin.earl.taylor@gmail.com>
2020-10-10 08:26:41 -0700

    fixed plotting bugs and added colormap option (version 0.0.2)

--- bjsfm/plotting.py
+++ bjsfm/plotting.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_stress(lekhnitskii_obj, comp=0, rnum=100, tnum=100, xbounds=None, ybounds=None, cmap='seismic'):
+def plot_stress(lek_1, lek_2=None, comp=0, rnum=100, tnum=100,
+                xbounds=None, ybounds=None, cmap='jet', cmin=None, cmax=None):
     """ Plots stresses
 
     Notes
@@ -12,7 +13,9 @@ def plot_stress(lekhnitskii_obj, comp=0, rnum=100, tnum=100, xbounds=None, yboun
 
     Parameters
     ----------
-    lekhnitskii_obj : :obj: lekhnitskii.UnloadedHole or :obj: lekhnitskii.LoadedHole
+    lek_1 : :obj: lekhnitskii.UnloadedHole or :obj: lekhnitskii.LoadedHole
+        LoadedHole or UnloadedHole instance
+    lek_2 : :obj: lekhnitskii.UnloadedHole or :obj: lekhnitskii.LoadedHole, optional
         LoadedHole or UnloadedHole instance
     comp : {0, 1, 2}, optional
         stress component, default=0
@@ -24,11 +27,15 @@ def plot_stress(lekhnitskii_obj, comp=0, rnum=100, tnum=100, xbounds=None, yboun
         (x0, x1) x-axis bounds, default=6*radius
     ybounds : tuple of int, optional
         (y0, y1) y-axis bounds default=6*radius
-    cmap : {'coolwarm', 'bwr', 'seismic', ...}, optional
+    cmap : :obj: matplotlib.colors.ColorMap, optional
         any colormap name from matplotlib.pyplot
+    cmin : float, optional
+        minimum value for colormap
+    cmax : float, optional
+        maximum value for colormap
 
     """
-    radius = lekhnitskii_obj.r
+    radius = lek_1.r
 
     xbounds = xbounds if xbounds else [-6*radius, 6*radius]
     ybounds = ybounds if ybounds else [-6*radius, 6*radius]
@@ -43,20 +50,27 @@ def plot_stress(lekhnitskii_obj, comp=0, rnum=100, tnum=100, xbounds=None, yboun
     y = np.array(radiis) * np.sin(thetas)
     x.shape = y.shape = (tnum, rnum)
 
-    stress = lekhnitskii_obj.stress(x.flatten(), y.flatten())[:, comp]
+    stress = lek_1.stress(x.flatten(), y.flatten())[:, comp]
+    if lek_2:
+        assert lek_1.r == lek_2.r, "Cannot plot plates with different radii."
+        stress_2 = lek_2.stress(x.flatten(), y.flatten())[:, comp]
+        stress += stress_2
+
     stress.shape = (tnum, rnum)
 
     fig, ax = plt.subplots()
     ax.set_aspect('equal')
-    cp = plt.contourf(x, y, stress, corner_mask=True, cmap=plt.get_cmap(cmap))
+    # color limits
+    cmin = cmin if cmin else np.min(stress)
+    cmax = cmax if cmax else np.max(stress)
+    levels = np.linspace(cmin, cmax, num=11, endpoint=True)
+    cp = plt.contourf(x, y, stress, levels, corner_mask=True, cmap=plt.get_cmap(cmap), extend='both')
     plt.colorbar(cp)
+    # graph limits
     plt.xlim(xbounds[0], xbounds[1])
     plt.ylim(ybounds[0], ybounds[1])
     plt.title(f'Python bjsfm Stress:\n {comp} dir stress')
     plt.show()
-
-
-
 
 
 
