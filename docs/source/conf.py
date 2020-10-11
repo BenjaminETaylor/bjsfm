7d00653ebcb2562923acbaf5305df3687b1c51d2
BenjaminETaylor <benjamin.earl.taylor@gmail.com>
2020-10-11 16:30:04 -0700

    added master_doc to sphinx conf.py for readthedocs (0.1.2)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -58,3 +59,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Change the expectation of top-level file for readthedocs to 'index' instead
+# of 'contents'
+master_doc = 'index'
