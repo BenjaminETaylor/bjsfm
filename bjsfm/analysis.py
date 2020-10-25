01be2c5080b7a1ccbccc7ec050acfef9da067d3f
BenjaminETaylor <benjamin.earl.taylor@gmail.com>
2020-10-25 16:49:37 -0700

    reorganized allowables input (0.2.0)

--- bjsfm/analysis.py
+++ bjsfm/analysis.py
@@ -13,12 +13,30 @@ class MaxStrain:
         hole diameter
     a_matrix : array_like
         2D 3x3 inverse A-matrix from CLPT
-    tension : array_like, optional
-        1D 1x4 array [et0, et45, et90, et-45] tension strain allowables in all four directions
-    compression : array_like, optional
-        1D 1x4 array [ec0, ec45, ec90, ec-45] compression strain allowables in all four directions
-    shear : array_like, optional
-        1D 1x4 array [es0, es45, es90, es-45] shear strain allowables in all four directions
+    et0 : float
+        tension strain allowable in 0 deg direction
+    et90 : float
+        tension strain allowable in 90 deg direction
+    et45 : float
+        tension strain allowable in 45 deg direction
+    etn45 : float
+        tension strain allowable in -45 deg direction
+    ec0 : float
+        compression strain allowable in 0 deg direction
+    ec90 : float
+        compression strain allowable in 90 deg direction
+    ec45 : float
+        compression strain allowable in 45 deg direction
+    ecn45 : float
+        compression strain allowable in -45 deg direction
+    es0 : float
+        shear strain allowable in 0 deg direction
+    es90 : float
+        shear strain allowable in 90 deg direction
+    es45 : float
+        shear strain allowable in 45 deg direction
+    esn45 : float
+        shear strain allowable in -45 deg direction
 
     Attributes
     ----------
@@ -30,23 +48,41 @@ class MaxStrain:
         2D 3x3 A-matrix from CLPT
     a_inv : ndarray
         2D 3x3 Inverse A-matrix from CLPT
-    tens : ndarray
-        1D 1x4 array [et0, et45, et90, et-45] tension strain allowables in all four directions
-    comp : ndarray
-        1D 1x4 array [ec0, ec45, ec90, ec-45] compression strain allowables in all four directions
-    shear : ndarray
-        1D 1x4 array [es0, es45, es90, es-45] shear strain allowables in all four directions
+    et0 : float
+        tension strain allowable in 0 deg direction
+    et90 : float
+        tension strain allowable in 90 deg direction
+    et45 : float
+        tension strain allowable in 45 deg direction
+    etn45 : float
+        tension strain allowable in -45 deg direction
+    ec0 : float
+        compression strain allowable in 0 deg direction
+    ec90 : float
+        compression strain allowable in 90 deg direction
+    ec45 : float
+        compression strain allowable in 45 deg direction
+    ecn45 : float
+        compression strain allowable in -45 deg direction
+    es0 : float
+        shear strain allowable in 0 deg direction
+    es90 : float
+        shear strain allowable in 90 deg direction
+    es45 : float
+        shear strain allowable in 45 deg direction
+    esn45 : float
+        shear strain allowable in -45 deg direction
 
     """
 
-    def __int__(self, thickness, diameter,  a_matrix, tension=None, compression=None, shear=None):
+    def __init__(self, thickness, diameter,  a_matrix, et0=None, et90=None, et45=None, etn45=None, ec0=None, ec90=None,
+                 ec45=None, ecn45=None, es0=None, es90=None, es45=None, esn45=None):
         self.t = thickness
         self.r = diameter/2.
         self.a = np.array(a_matrix)
         self.a_inv = np.linalg.inv(self.a)
-        self.tens = np.array(tension) if tension else None
-        self.comp = np.array(compression) if compression else None
-        self.shear = np.array(shear) if shear else None
+        self.e_allow = {'et0': et0, 'et90': et90, 'et45': et45, 'etn45': etn45, 'ec0': ec0, 'ec90': ec90,
+                        'ec45': ec45, 'ecn45': ecn45, 'es0': es0, 'es90': es90, 'es45': es45, 'esn45': esn45}
 
     def _radial_points(self, step, num_pnts):
         """Calculates x, y points"""
@@ -57,19 +93,9 @@ class MaxStrain:
         return x, y
 
     @staticmethod
-    def _strain_margins(strains, e1t=None, e1c=None, e2t=None, e2c=None, e12=None):
+    def _strain_margins(strains):
         """Calculates margins of safety"""
-        margins = [np.nan]*3
-        if e1t and e1c:
-            # 0 deg
-            margins[0] = e1t/strains[0] - 1 if strains[0] > 0. else e1c/strains[0] - 1
-        if e2t and e2c:
-            # 90 deg
-            margins[1] = e2t/strains[1] - 1 if strains[1] > 0. else e2c/strains[1] - 1
-        if e12:
-            # shear
-            margins[2] = e12/strains[2] - 1
-        return margins
+        raise NotImplementedError("Oops! Haven't implemented this yet.")
 
     def stresses(self, bearing, bypass, rc=0., num=100):
         """ Calculate stresses
@@ -94,7 +120,7 @@ class MaxStrain:
         d = self.r*2
         t = self.t
         a_inv = self.a_inv
-        alpha = np.tan(bearing[1]/bearing[0])
+        alpha = np.tan(bearing[1]/bearing[0]) if abs(bearing[0]) > 0. else 0.
         p = np.sqrt(np.sum(np.square(bearing)))
         x, y = self._radial_points(rc, num)
         brg = lek.LoadedHole(p, d, t, a_inv, theta=alpha)
@@ -102,6 +128,30 @@ class MaxStrain:
         byp_stress = byp.stress(x, y)
         brg_stress = brg.stress(x, y)
         return byp_stress + brg_stress
+
+    def strains(self, bearing, bypass, rc=0., num=100):
+        """ Calculate strains
+
+        Parameters
+        ----------
+        bearing : array_like
+            1D 1x2 array bearing load [Px, Py] (force)
+        bypass : array_like
+            1D 1x3 array bypass loads [Nx, Ny, Nxy] (force/unit-length)
+        rc : float, default 0.
+            characteristic distance
+        num : int, default 100
+            number of points to check around hole
+
+        Returns
+        -------
+        ndarray
+            2D numx3 array of plate strains
+
+        """
+        stresses = self.stresses(bearing, bypass, rc=rc, num=num)
+        strains = self.a_inv @ stresses.T/self.t
+        return strains.T
 
     def analyze(self, bearing, bypass, rc=0., num=100):
         """ Analyze the joint for a set of loads
@@ -120,17 +170,11 @@ class MaxStrain:
         Returns
         -------
         ndarray
-            2D numx3 array of margins of safety
+            2D numx6 array of margins of safety
 
         """
         margins = []
-        a_inv = self.a_inv
-        stresses = self.stresses(bearing, bypass, rc=rc, num=num)
-        for stress in stresses:
-            # x-direction
-            strain = a_inv.dot(stress/self.t)
-            e1t, e1c, e2t, e2c, e12 = self.tens[0], self.comp[0], self.tens[2], self.comp[2], self.shear[0]
-            margins.append(self._strain_margins(strain, e1t=e1t, e1c=e1c, e2t=e2t, e2c=e2c, e12=e12))
-        return margins
+        strains = self.strains(bearing, bypass, rc=rc, num=num)
+        return self._strain_margins(strains)
 
 
