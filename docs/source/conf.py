25c5816dd10c66d920be8e39786d3ca01c6468ac
BenjaminETaylor <benjaminearltaylor@gmail.com>
2022-10-14 15:08:11 -0700

    readthedocs not working, tried adding autodoc_mock_imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# required imports
+autodoc_mock_imports = ['numpy', 'matplotlib', 'nptyping']
+
 
 # -- Options for HTML output -------------------------------------------------
 
